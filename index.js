7209836f6d18a5bc04427a19de9f6b13985cc1c9
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-05-24 08:46:00 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/* 
+WAP JavaScript function that reverse a number.
+*/
+
+function reverse_a_number(n)
+  {
+    n = n + "";
+    return n.split("").reverse().join("");
+  }
+
+let num = prompt("Enter Number to Reverse: ");
+console.log(reverse_a_number(num));
